ee0de4feb36992b8e55a03e87c1739c827b42daf
davelinio <david.schiefer@me.com>
2012-07-16 22:52:44 +0100

    Initial Commit

---
+++ DASelectableToolbarController.h
@@ -0,0 +1,39 @@
+//
+//  DASelectableToolbarController.h
+//  Cookie Stumbler
+//
+//  Created by David Schiefer on 16.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+enum 
+{
+    DASelectableToolbarLayoutDefault = 0, /* || ITEM | ITEM || */
+    DASelectableToolbarLayoutCentered = 1 /* || SEPERATOR | ITEM | ITEM | SEPERATOR || */
+};
+typedef NSUInteger DASelectableToolbarLayout;
+
+@interface DASelectableToolbarController : NSTabView <NSToolbarDelegate>
+{
+    @private
+    NSToolbar*_toolBar;
+    id _toolbarItems;
+    
+    NSUInteger _layout;
+    
+    NSWindow*_hostWindow;
+}
+
+- (void)addToolbarItem:(NSToolbarItem *)item;
+- (void)removeToolbarItem:(NSToolbarItem *)item;
+
+- (void)selectItemAtIndex:(NSUInteger)index;
+
+@property (assign) IBOutlet NSWindow*window;
+@property (assign) DASelectableToolbarLayout layout; /* Default - DASelectableToolbarLayoutCentered */
+
+@property (readonly) NSToolbar*toolbar;
+
+@end
---
+++ DASelectableToolbarController.m
@@ -0,0 +1,189 @@
+//
+//  DASelectableToolbarController.m
+//  Cookie Stumbler
+//
+//  Created by David Schiefer on 16.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DASelectableToolbarController.h"
+
+@implementation DASelectableToolbarController
+
+@synthesize layout = _layout;
+
+- (void)setWindow:(NSWindow *)window
+{
+    /* Default Layout */
+    _layout = DASelectableToolbarLayoutCentered;
+    
+    /* Prepare Required Objects */
+    _toolbarItems = [[NSMutableArray alloc] init];
+    _toolBar = [[NSToolbar alloc] initWithIdentifier:@"Identifier"];
+    
+    _hostWindow = [window retain];
+    
+    /* Configure NSTabView */
+    [self setTabViewType:NSNoTabsNoBorder];
+    
+    /* Internal Setup */
+    [self _setupToolbar:_toolBar];
+    
+    [_toolBar setDelegate:self];
+    [window setToolbar:_toolBar];
+    
+    /* Select 1st Item */
+    [self selectItemAtIndex:1];
+}
+
+- (NSWindow *)window
+{
+    return _hostWindow;
+}
+
+- (void)selectItemAtIndex:(NSUInteger)index
+{
+    NSToolbarItem*_relevantItem = [_toolbarItems objectAtIndex:index];
+    
+    if (_relevantItem != nil)
+    {
+        [_toolBar setSelectedItemIdentifier:[_relevantItem itemIdentifier]];
+        [_hostWindow setTitle:[_relevantItem label]];
+        
+        [self selectTabViewItemWithIdentifier:[_relevantItem itemIdentifier]];
+    }
+}
+
+- (void)addToolbarItem:(NSToolbarItem *)item
+{
+    if ([item class] == [NSToolbarItem class])
+    {
+        [_toolbarItems addObject:item];
+    }
+}
+
+- (void)removeToolbarItem:(NSToolbarItem *)item
+{
+    [_toolbarItems removeObject:item];
+}
+
+- (void)setLayout:(DASelectableToolbarLayout)layout
+{
+    [_toolbarItems removeAllObjects];
+    [self _setupToolbar:_toolBar];
+}
+
+- (DASelectableToolbarLayout)layout
+{
+    return _layout;
+}
+
+- (NSToolbar *)toolbar
+{
+    return _toolBar;
+}
+
+#pragma mark Internal Functionality
+
+- (void)_setupToolbar:(NSToolbar *)toolbar
+{
+    if (self.layout == DASelectableToolbarLayoutCentered)
+    {
+        [self addToolbarItem:[self _flexibleSpaceItem]];
+    }
+    
+    for (NSTabViewItem*_item in self.tabViewItems)
+    {
+        NSToolbarItem*_toolbarItem = [[NSToolbarItem alloc] initWithItemIdentifier:_item.identifier];
+        [_toolbarItem setLabel:_item.label];
+        [_toolbarItem setPaletteLabel:_item.label];
+        [_toolbarItem setImage:[NSImage imageNamed:_item.identifier]];
+        
+        [_toolbarItem setTarget:self];
+        [_toolbarItem setAction:@selector(_switchView:)];
+        
+        [self addToolbarItem:_toolbarItem];
+        [_toolbarItem release];
+    }
+    
+    if (self.layout == DASelectableToolbarLayoutCentered)
+    {
+        [self addToolbarItem:[self _flexibleSpaceItem]];
+    }
+}
+
+- (NSToolbarItem *)_flexibleSpaceItem
+{
+    return [[[NSToolbarItem alloc] initWithItemIdentifier:NSToolbarFlexibleSpaceItemIdentifier] autorelease];
+}
+
+- (void)_switchView:(id)sender
+{
+    [self selectItemAtIndex:[_toolbarItems indexOfObject:sender]];
+}
+
+#pragma mark NSToolbarDelegates
+
+- (NSArray *)toolbarSelectableItemIdentifiers: (NSToolbar *)toolbar
+{
+    NSMutableArray*_identifiers = [[NSMutableArray alloc] init];
+    
+    for (NSToolbarItem*_item in _toolbarItems)
+    {
+        [_identifiers addObject:_item.itemIdentifier];
+    }
+    
+    return [_identifiers autorelease];
+}
+
+- (NSArray *)toolbarDefaultItemIdentifiers:(NSToolbar *)toolbar 
+{
+    NSMutableArray*_identifiers = [[NSMutableArray alloc] init];
+    
+    for (NSToolbarItem*_item in _toolbarItems)
+    {
+        [_identifiers addObject:_item.itemIdentifier];
+    }
+    
+    return [_identifiers autorelease];
+}
+
+- (NSArray *)toolbarAllowedItemIdentifiers:(NSToolbar*)toolbar
+{
+    NSMutableArray*_identifiers = [[NSMutableArray alloc] init];
+    
+    for (NSToolbarItem*_item in _toolbarItems)
+    {
+        [_identifiers addObject:_item.itemIdentifier];
+    }
+
+    return [_identifiers autorelease];
+}
+
+- (NSToolbarItem *)toolbar:(NSToolbar *)toolbar itemForItemIdentifier:(NSString *)itemIdentifier willBeInsertedIntoToolbar:(BOOL)flag
+{
+    for (NSToolbarItem*_item in _toolbarItems)
+    {
+        if ([_item.itemIdentifier isEqualToString:itemIdentifier])
+        {
+            return _item;
+        }
+    }
+    
+    return nil;
+}
+
+- (void)dealloc
+{
+    [_toolBar setDelegate:nil];
+    [_toolBar release];
+    
+    [_hostWindow setToolbar:nil];
+    [_hostWindow release];
+
+    [_toolbarItems release];
+    
+    [super dealloc];
+}
+
+@end
